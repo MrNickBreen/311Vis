feeff174eb6275fa63d07f8229286c77a19bfa0d
davidrs <drustsmith@drustsmith-macbookpro.roam.corp.google.com>
2016-02-24 18:28:14 -0800

    Have basic data fetch.

--- scrap/app.js
+++ scrap/app.js
@@ -1,6 +1,9 @@
 // Entry point to the entire app.
 var start = function(){
-	loadData();
+	var FOUR_WEEKS = 4 * 7;
+	loadData(FOUR_WEEKS).then(function(data){
+		console.log("drs2 ", data);
+	});
 };
 
 /*
@@ -9,8 +12,12 @@ Sample JSON API call: https://data.sfgov.org/resource/vw6y-z8j6.json?$limit=2000
 Data Sample in Sheet: https://docs.google.com/spreadsheets/d/1yGd2B7F8mlDg64L-uL3Byuxfxn1irl4ZLd0vETaq2GU/edit#gid=1562030977 
 Socrata API docs: https://dev.socrata.com/consumers/getting-started.html
 */
-var loadData = function(){
-	
+var loadData = function(timeperiodDays){
+	var fitlers = {}; //TODO: add filters so it gets the last timeperiodDays worth of data
+	return $.getJSON('https://data.sfgov.org/resource/vw6y-z8j6.json?$limit=2000', function(data){
+		console.log('data', data);
+		// TODO anything we want....but try to leave agregating out of the load function.
+	});
 };
 
 start();
