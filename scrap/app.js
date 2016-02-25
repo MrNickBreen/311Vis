9f20b0a711b6e7e298516a54d3b346901eaefe08
davidrs <drustsmith@drustsmith-macbookpro.roam.corp.google.com>
2016-02-24 20:11:14 -0800

    Add basic comparison

--- scrap/app.js
+++ scrap/app.js
@@ -13,7 +13,8 @@ var start = function(){
 		aggregatedRecentWindow = aggregateData(recentWindow);
 		aggregatedOldWindow = aggregateData(oldWindow);
 		console.log('aggregatedOldWindow', aggregatedOldWindow);
-		// TODO: comparisonList = compareWindows(aggregatedOldWindow, aggregatedRecentWindow);
+		comparisonList = compareWindows(aggregatedRecentWindow, aggregatedOldWindow);
+		console.log(comparisonList);
 		// compare return must already be sorted.
 		// comparisonList = [{agency: "blah ", change: 0.45, newValue: 12},...]
 		//TODO: drawChart(comparisonList);
@@ -33,38 +34,52 @@ var loadData = function(){
         begginingOfAnalysisWindow = todaysMomentDate.subtract(ANALYSIS_WINDOW_IN_DAYS*2, 'days').format('YYYY-MM-DD'),
         filters = "$where=(closed > \'"+begginingOfAnalysisWindow+"\' AND closed < \'"+todaysDate+"\') OR (opened > \'"+begginingOfAnalysisWindow+"\' AND opened < \'"+todaysDate+"\')";
 
-	return $.getJSON('https://data.sfgov.org/resource/vw6y-z8j6.json?$limit=50000&'+filters, function(data){
+	return $.getJSON('https://data.sfgov.org/resource/vw6y-z8j6.json?$limit=500&'+filters, function(data){
 		console.log('data', data);
 		// do anything we want....but try to leave agregating out of the load function.
 	});
 };
 
+// returns [{responsible_agency: "blah ", change: 0.45, newValue: 12},...]
+var compareWindows = function(recentW, oldW){
+	var comparisonList = [];
+	_.each(recentW, function(data, agencyName){
+		var oldRate = oldW[agencyName].closed;
+		var recentRate = recentW[agencyName].closed;
+		comparisonList.push({responsible_agency: agencyName, 
+			changeInCloseRate: recentRate - oldRate,
+			recentCloseRate: recentRate,
+			oldCloseRate: oldRate})
+	});
+
+	_.sortBy(comparisonList, function(agency){ return agency.changeInCloseRate; });
+
+	return comparisonList;
+}
 
 // Aggregate data to the responsible agency.
 // TODO3: instead of always doing responsible agency, make aggregateable key a param.
-var aggregateData = function(data, timeWindow) {
+var aggregateData = function(dataWindow) {
 	var STATUS_CLOSED = "Closed";
 	// format: {"agency Name blah": {responsible_agency: "blah", newlyOpened:123, closed:456}}
 	var aggregatedData = {};
 
-	data.forEach(function(d){
+	dataWindow.forEach(function(d){
 		var agency = d.responsible_agency;
 		// first time we see this agency
 		if(!aggregatedData[agency]){
 			aggregatedData[agency] = {responsible_agency: agency, newlyOpened: 0, closed: 0};
 		}
-		if (d.status === STATUS_CLOSED) {
+		if (moment(d.closed).isBetween(dataWindow.start, dataWindow.end)) {
 			aggregatedData[agency].closed++;
 		}
-		//if (moment.isIn(d.opened ,timeWindow)){ // TODO: use moment to check if it isInNowWindow
-			d.newlyOpened++;
-//		}
+		if (moment(d.opened).isBetween(dataWindow.end, dataWindow.end)){ // TODO: use moment to check if it isInNowWindow
+			aggregatedData[agency].newlyOpened++;
+		}
 	});
 
 	return aggregatedData;
 }
-
-
 
 // ===== nick write below here
 
