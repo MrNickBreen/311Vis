fc845cf8433645da04c633ba5f550e62e5a45d2a
Nick Breen <nick@nickbreen.ca>
2016-02-24 19:35:56 -0800

    adds basic moment and first attempt at date filter

--- scrap/app.js
+++ scrap/app.js
@@ -3,14 +3,14 @@ var ANALYSIS_WINDOW_IN_DAYS = 14;
 // Entry point to the entire app.
 var start = function(){
 
- 
+
 
 	loadData().then(function(data){
 		console.log("drs2 ", data);
 		// TODONICK: split data into two arrays. 'first half and second half'
 		recentWindow = data;
 		oldWindow = data;
-		// recentWindow is today to today-ANALYSIS_WINDOW_IN_DAYS  and oldWindow = today-ANALYSIS_WINDOW_IN_DAYS  to today-2*ANALYSIS_WINDOW_IN_DAYS 
+		// recentWindow is today to today-ANALYSIS_WINDOW_IN_DAYS  and oldWindow = today-ANALYSIS_WINDOW_IN_DAYS  to today-2*ANALYSIS_WINDOW_IN_DAYS
 		//DONT DISTRUCT PARAMS PASSED INTO AGGREGATE FUNCTIONS.
 		// TODO: DAVID
 		aggregatedRecentWindow = aggregateData(recentWindow);
@@ -25,19 +25,22 @@ var start = function(){
 
 
 /*
-	Data: https://data.sfgov.org/City-Infrastructure/Case-Data-from-San-Francisco-311-SF311-/vw6y-z8j6 
-	Sample JSON API call: https://data.sfgov.org/resource/vw6y-z8j6.json?$limit=2000 
-	Data Sample in Sheet: https://docs.google.com/spreadsheets/d/1yGd2B7F8mlDg64L-uL3Byuxfxn1irl4ZLd0vETaq2GU/edit#gid=1562030977 
+	Data: https://data.sfgov.org/City-Infrastructure/Case-Data-from-San-Francisco-311-SF311-/vw6y-z8j6
+	Sample JSON API call: https://data.sfgov.org/resource/vw6y-z8j6.json?$limit=2000
+	Data Sample in Sheet: https://docs.google.com/spreadsheets/d/1yGd2B7F8mlDg64L-uL3Byuxfxn1irl4ZLd0vETaq2GU/edit#gid=1562030977
 	Socrata API docs: https://dev.socrata.com/consumers/getting-started.html
 */
 var loadData = function(){
-	// TODO NICK: 
+	// TODO NICK:
 	//   get todays date
 	//   todays date - ANALYSIS_WINDOW_IN_DAYS*2
 	// turn into filter object and pass it in the request.
 
-	var fitlers = {}; //TODO: add filters to request
-	return $.getJSON('https://data.sfgov.org/resource/vw6y-z8j6.json?$limit=2000', function(data){
+	var todaysDate = moment().startOf('day'),
+        begginingOfAnalysisWindow = todaysDate.subtract(ANALYSIS_WINDOW_IN_DAYS*2, 'days'),
+        filters = "$where=(opened > \'2016-02-02\' AND opened < \'2016-02-24\') OR (closed > \'2016-02-02\' AND closed < \'2016-02-24\')" ; //TODO: add filters to request
+
+	return $.getJSON('https://data.sfgov.org/resource/vw6y-z8j6.json?$limit=50000&'+filters, function(data){
 		console.log('data', data);
 		// do anything we want....but try to leave agregating out of the load function.
 	});
