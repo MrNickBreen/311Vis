e697194bca6a6b95e21afead96a24687d4495aac
davidrs <drustsmith@drustsmith-macbookpro.roam.corp.google.com>
2016-02-24 18:21:21 -0800

    Add scrappy template

---
+++ scrap/app.js
@@ -0,0 +1,16 @@
+// Entry point to the entire app.
+var start = function(){
+	loadData();
+};
+
+/*
+Data: https://data.sfgov.org/City-Infrastructure/Case-Data-from-San-Francisco-311-SF311-/vw6y-z8j6 
+Sample JSON API call: https://data.sfgov.org/resource/vw6y-z8j6.json?$limit=2000 
+Data Sample in Sheet: https://docs.google.com/spreadsheets/d/1yGd2B7F8mlDg64L-uL3Byuxfxn1irl4ZLd0vETaq2GU/edit#gid=1562030977 
+Socrata API docs: https://dev.socrata.com/consumers/getting-started.html
+*/
+var loadData = function(){
+	
+};
+
+start();
