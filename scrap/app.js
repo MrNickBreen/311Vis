193d7ff58e8a81ca6e3371babaeaf77c3db9434f
davidrs <drustsmith@drustsmith-macbookpro.roam.corp.google.com>
2016-02-26 07:14:08 -0800

    Delete old code, switch to rate %, improve layout

--- scrap/app.js
+++ scrap/app.js
@@ -1,4 +1,5 @@
-var ANALYSIS_WINDOW_IN_DAYS = 7,
+var ANALYSIS_WINDOW_IN_DAYS = 14,
+	MIN_CLOSES = 40, // both weeks must have at least this many closes to consider the department.
     todaysDate = moment().startOf('day').format('YYYY-MM-DD'),
     begginingOfAnalysisWindow = moment().startOf('day').subtract((ANALYSIS_WINDOW_IN_DAYS*2)-1, 'days').format('YYYY-MM-DD'),
 
@@ -16,37 +17,11 @@ var start = function(){
 	var aPromise = loadData(oldWindow);
 	var bPromise = loadData(recentWindow);
 	$.when(aPromise, bPromise).then(function(){
-		console.log("when drs2 ");
-        // NOTE: split data into two arrays, an event can be opened in oldWindow
-		// and closed in recentWindow
-        // $.each( data, function( i, entry ) {
-        //     var eventIsOpenedBetweenRecentStartAndEnd =
-        //         moment(entry.opened).isAfter(recentWindow.start) &&
-        //         moment(entry.opened).isBefore(recentWindow.end),
-        //         eventIsClosedBetweenRecentStartAndEnd =
-        //         moment(entry.closed).isAfter(recentWindow.start) &&
-        //         moment(entry.closed).isBefore(recentWindow.end);
-        //   if (eventIsOpenedBetweenRecentStartAndEnd ||
-        //     eventIsClosedBetweenRecentStartAndEnd) {
-        //         recentWindow.data.push(entry);
-        //   } else {
-        //       oldWindow.data.push(entry);
-        //   }
-        // })
-		// oldWindow = today-(2*ANALYSIS_WINDOW_IN_DAYS) to today-ANALYSIS_WINDOW_IN_DAYS
-		// recentWindow is today-ANALYSIS_WINDOW_IN_DAYS+1 to today
-		// NOTE: DONT DISTRUCT PARAMS PASSED INTO AGGREGATE FUNCTIONS.
-		// TODO: DAVID
-		// aggregatedRecentWindow = aggregateData(recentWindow);
-		// aggregatedOldWindow = aggregateData(oldWindow);
-		console.log('oldWindow', oldWindow);
-		console.log('recentWindow', recentWindow);
 		comparisonList = compareWindows(recentWindow.data, oldWindow.data);
-		console.log('comaprisonList', comparisonList);
 		// compare return must already be sorted.
-		// comparisonList = [{agency: "blah ", change: 0.45, newValue: 12},...]
-		comparisonList = filterComparisonData(comparisonList);
-		drawChart(comparisonList);
+		// comparisonList type: [{agency: "blah ", change: 0.45, newValue: 12},...]
+		filteredList = filterComparisonData(comparisonList);
+		drawChart(filteredList);
 	});
 };
 
@@ -58,10 +33,7 @@ var start = function(){
 	Socrata API docs: https://dev.socrata.com/consumers/getting-started.html
 */
 var loadData = function(window){
-	// TODO: replace with calls like this:
 	var closedFilter = "$select=COUNT(*),responsible_agency&$group=responsible_agency&$where=(closed > \'"+window.start+"\' AND closed < \'"+window.end+"\')";
-
-	//var filters = "$where=(closed > \'"+begginingOfAnalysisWindow+"\' AND closed < \'"+todaysDate+"\')";//" OR (opened > \'"+begginingOfAnalysisWindow+"\' AND opened < \'"+todaysDate+"\')";
 
 	return $.getJSON('https://data.sfgov.org/resource/vw6y-z8j6.json?$limit=50000&' + closedFilter, function(data){
 		var map = {};
@@ -81,7 +53,8 @@ var compareWindows = function(recentW, oldW){
 		var oldRate = oldW[agencyName] ? oldW[agencyName].closed : 0;
 		var recentRate = recentW[agencyName] ? recentW[agencyName].closed : 0;
 		comparisonList.push({responsible_agency: agencyName,
-			changeInCloseRate: recentRate - oldRate,
+			changeInCloseCount: recentRate - oldRate,
+			changeInCloseRate: (recentRate - oldRate) / oldRate,
 			recentCloseRate: recentRate,
 			oldCloseRate: oldRate})
 	});
@@ -91,11 +64,39 @@ var compareWindows = function(recentW, oldW){
 	return comparisonList.reverse();
 }
 
+
+var filterComparisonData = function(comparisonList){
+	var newList = _.reject(comparisonList, function(item){
+		return item.recentCloseRate < MIN_CLOSES || item.oldCloseRate < MIN_CLOSES;
+	});
+	return newList;
+};
+
+var drawChart = function(comparisonList){
+    $('#leaderboard').html('');
+	comparisonList.forEach(function(agency){
+        var isGood = agency.changeInCloseRate > 0,
+            classColor = isGood ? 'good': 'bad';
+
+		$('#leaderboard').append("<div class='agency'>" + 
+			agency.responsible_agency + "  " +
+        	"<span class='change-num "+classColor+"'>" +
+	        	"<span class='small'>"+agency.oldCloseRate + " -> " + agency.recentCloseRate+"</span> "+
+	        	prettyPercent(agency.changeInCloseRate) + "% "+        	  
+        	"</span>" +
+        "</div>")
+	});
+
+};
+
+var prettyPercent = function(num){
+	return Math.round(num * 100);
+};
+
 // Aggregate data to the responsible agency.
-// TODO3: instead of always doing responsible agency, make aggregateable key a param.
-var aggregateData = function(dataWindow) {
+// format: {"agency Name blah": {responsible_agency: "blah", newlyOpened:123, closed:456}}
+var UNUSED_aggregateData = function(dataWindow) {
 	var STATUS_CLOSED = "Closed";
-	// format: {"agency Name blah": {responsible_agency: "blah", newlyOpened:123, closed:456}}
 	var aggregatedData = {};
 
 	dataWindow.data.forEach(function(d){
@@ -114,29 +115,6 @@ var aggregateData = function(dataWindow) {
 
 	return aggregatedData;
 }
-
-var filterComparisonData = function(comparisonList){
-	var MIN_CLOSES = 10;
-	var newList = _.reject(comparisonList, function(item){
-		return item.recentCloseRate < MIN_CLOSES || item.oldCloseRate < MIN_CLOSES;
-	});
-	return newList;
-};
-
-var drawChart = function(comparisonList){
-    $('#leaderboard').html('');
-	comparisonList.forEach(function(agency){
-        var isGood = agency.changeInCloseRate > 0,
-            classColor = isGood ? 'good': 'bad';
-		$('#leaderboard').append("<div class='agency'>" + agency.responsible_agency + "  " +
-        "<span class='change-num "+classColor+"'>" +
-        agency.changeInCloseRate + "</span></div>")
-	});
-
-};
-
-// ===== nick write below here
-
 
 
 start();
